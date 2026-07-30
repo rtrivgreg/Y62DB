60e75ad27dff4ca96510a665ad77473238c59692
rtrivgreg <rtrivgreg@yahoo.com>
2026-07-30 16:13:38 -0400

    Create DynamoDB tables for config rules and parameters
    Added two DynamoDB tables for config rules and parameters with point-in-time recovery and server-side encryption enabled.

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,1 +1,43 @@
+resource "aws_dynamodb_table" "config_rules" {
+  name         = var.config_rules_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "rule_id"
 
+  attribute {
+    name = "rule_id"
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+}
+
+resource "aws_dynamodb_table" "config_rule_parameters" {
+  name         = var.config_rule_parameters_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "rule_id"
+  range_key    = "parameter_name"
+
+  attribute {
+    name = "rule_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "parameter_name"
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+}
