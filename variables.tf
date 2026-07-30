f5ffbab6bb4f68392b33fcf309ff7e6389042644
rtrivgreg <rtrivgreg@yahoo.com>
2026-07-30 16:07:45 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,21 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region for DynamoDB tables."
+  default     = "us-east-1"
+}
 
+variable "environment" {
+  type        = string
+  description = "Environment name."
+  default     = "dev"
+}
+
+variable "config_rules_table_name" {
+  type        = string
+  default     = "config_rules"
+}
+
+variable "config_rule_parameters_table_name" {
+  type        = string
+  default     = "config_rule_parameters"
+}
