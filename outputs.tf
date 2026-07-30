eb6023749e1dd9f6bbf30937f30e648eb15344b6
rtrivgreg <rtrivgreg@yahoo.com>
2026-07-30 16:15:17 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,15 @@
+output "config_rules_table_name" {
+  value = aws_dynamodb_table.config_rules.name
+}
 
+output "config_rules_table_arn" {
+  value = aws_dynamodb_table.config_rules.arn
+}
+
+output "config_rule_parameters_table_name" {
+  value = aws_dynamodb_table.config_rule_parameters.name
+}
+
+output "config_rule_parameters_table_arn" {
+  value = aws_dynamodb_table.config_rule_parameters.arn
+}
