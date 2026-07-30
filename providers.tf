aea331b03737f32f2a7a673130a6d9112705d748
rtrivgreg <rtrivgreg@yahoo.com>
2026-07-30 16:06:36 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,11 @@
+provider "aws" {
+  region = var.aws_region
 
+  default_tags {
+    tags = {
+      Project     = "Y62DB"
+      ManagedBy   = "terraform"
+      Environment = var.environment
+    }
+  }
+}
