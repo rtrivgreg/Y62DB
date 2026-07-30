79c22c575e0717eaa23270475df15b0a8fe1b18d
rtrivgreg <rtrivgreg@yahoo.com>
2026-07-30 16:00:37 -0400

    Add Terraform configuration with AWS provider

--- terraform.tf
+++ terraform.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_version = ">= 1.8.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  cloud {
+    organization = "RSHL2136"
+
+    workspaces {
+      name = "Y62DB"
+    }
+  }
+}
