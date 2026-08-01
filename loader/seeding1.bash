d445c1dd5c389e0e0907980fcdb3dceabaea5734
Raymond Gregoire <rtrivgreg@yahoo.com>
2026-08-01 19:18:37 -0400

    Update seeding1.bash

--- loader/seeding1.bash
+++ loader/seeding1.bash
@@ -1,1 +1,8 @@
 aws dynamodb batch-write-item --request-items file://./seeding1.json --region us-east-1
+
+
+aws dynamodb query \
+  --table-name y62db-config-rule-catalog \
+  --key-condition-expression "pk = :pk" \
+  --expression-attribute-values '{":pk":{"S":"RULE#access-keys-rotated"}}' \
+  --region us-east-1
